744fe4fbdccb2b686d0a575f8680c7384afc6614
ocassio <orlicus@gmail.com>
2016-04-18 21:18:28 +0400

    Custom date section (not ready yet)

--- screens/DateScreen.js
+++ screens/DateScreen.js
@@ -31,11 +31,25 @@ class DateScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SegmentedControls
-          selectedOption={dateOptions[0]}
-          options={dateOptions}
-          direction='column'
-        />
+        <Text style={styles.header}>Диапазон</Text>
+        <View style={styles.dateOptionsContainer}>
+          <SegmentedControls
+            selectedOption={dateOptions[0]}
+            options={dateOptions}
+            direction='column'
+          />
+        </View>
+        <Text style={styles.header}>Выберите дату</Text>
+        <View style={styles.customDateContainer}>
+          <Text style={styles.customDateLabel}>От</Text>
+          <Text style={styles.customDateValue}>25.04.2016</Text>
+        </View>
+        <View style={styles.separator}/>
+        <View style={styles.customDateContainer}>
+          <Text style={styles.customDateLabel}>До</Text>
+          <Text style={styles.customDateValue}>30.04.2016</Text>
+        </View>
+        <View style={styles.separator}/>
       </View>
     );
   }
@@ -47,10 +61,37 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'stretch',
-    marginTop: 80,
-    marginLeft: 15,
-    marginRight: 15,
-    marginBottom: 15
+    marginTop: 64
+  },
+  header: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#eeeeee'
+  },
+  dateOptionsContainer: {
+    margin: 10
+  },
+  customDateContainer: {
+    flexDirection: 'row',
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  customDateLabel: {
+    marginRight: 10,
+    fontWeight: '500'
+  },
+  customDateValue: {
+    flex: 1,
+    textAlign: 'right',
+    fontWeight: '300'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#eeeeee'
   }
 });
 
